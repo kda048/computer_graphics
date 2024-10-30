0632c86e980b53e244e0dcd458ff7a85b1c88fee
kda048 <101637874+kda048@users.noreply.github.com>
2024-10-30 21:49:35 +0300

    Add files via upload

---
+++ task1.1/cg_laba1_1.cpp
@@ -0,0 +1,45 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <cmath>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat inputImage = imread("D:\\загрузкия_D\\flowers.png", IMREAD_GRAYSCALE);
+
+    if (inputImage.empty()) {
+        cout << "Ошибка: Не удалось загрузить изображение." << endl;
+        return 1;
+    }
+
+    Mat outputImage = Mat::zeros(inputImage.size(), CV_8UC1);
+
+    int centerX = inputImage.cols / 2;
+    int centerY = inputImage.rows / 2;
+
+
+    int maxRadius = min(centerX, centerY);
+
+
+    for (int y = 0; y < inputImage.rows; y++) {
+        for (int x = 0; x < inputImage.cols; x++) {
+            double distance = sqrt(pow(x - centerX, 2) + pow(y - centerY, 2));
+
+            if (distance <= maxRadius) {
+                outputImage.at<uchar>(y, x) = inputImage.at<uchar>(y, x);
+            }
+            else {
+                outputImage.at<uchar>(y, x) = 0;
+            }
+        }
+    }
+
+    imwrite("D:\\загрузкия_D\\lab_1.1_end.png", outputImage);
+
+    imshow("Круглое полутоновое изображение", outputImage);
+
+    waitKey(0);
+    return 0;
+}
+
