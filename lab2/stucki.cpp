bc7d34667c70667068210692d4f40f86778fbfeb
kda048 <101637874+kda048@users.noreply.github.com>
2025-01-11 22:12:02 +0300

    Update stucki.cpp

--- lab2/stucki.cpp
+++ lab2/stucki.cpp
@@ -2,31 +2,33 @@
 #include "floyd.h"
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <cmath>
+
 using namespace cv;
 using namespace std;
 
 
 void stucki_dithering_grayscale(Mat& img, int n) {
 
-    Mat out_img = Mat::zeros(img.size(), CV_8U);
 
-    
     int matrix[3][5] = {
         {0, 0, 0, 8, 4},
         {2, 4, 8, 4, 2},
         {1, 2, 4, 2, 1}
     };
 
-    
+
     for (int y = 0; y < img.rows; y++) {
         for (int x = 0; x < img.cols; x++) {
-            
+
             uchar pixel = img.at<uchar>(y, x);
-            
+
             uchar quantized_pixel = quantize_pixel(pixel, n);
-            
+
             int error = pixel - quantized_pixel;
-            
+            img.at<uchar>(y, x) = quantized_pixel; 
+
+
             for (int dy = -1; dy <= 1; dy++) {
                 for (int dx = -2; dx <= 2; dx++) {
                     if (x + dx >= 0 && x + dx < img.cols && y + dy >= 0 && y + dy < img.rows) {
@@ -37,12 +39,6 @@ void stucki_dithering_grayscale(Mat& img, int n) {
                     }
                 }
             }
-
-            out_img.at<uchar>(y, x) = quantized_pixel;
         }
     }
-
-    img = out_img;
 }
-
-
